046172b6cbea56ae14d4e89dbecf57511bb352ff
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-04-26 17:18:02 +0200

    fix: remove conflict

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
-<<<<<<< HEAD
-}
-=======
     public static void main(final String... args) {
         System.out.println("Hello World!");
     }
 }
->>>>>>> d7fe8d5da8ce50633fc6f2c03d232a2692f13cb4

8243c8a426bffcaad4c5002d8f779247c14538ba
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-04-26 22:24:31 +0200

    fix: fix gradle support for csv report jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,4 +27,7 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        csv.required.set(true)
+    }
 }
